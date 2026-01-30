e6287b59ba5f01ee1da0c7b8fcf77e22809edce5
CKlimeths <cklimeths@outlook.com>
2026-01-30 15:54:51 +0800

    build: 添加astra配置文件并优化构建流程

---
+++ astra.config.js
@@ -0,0 +1,10 @@
+export default {
+    outFile: "dist/excel-unlocker.exe",
+
+    modifyMetadata: true,
+
+    exe: {
+        productName: "Excel Unlocker",
+        productVersion: "0.1.0",
+    },
+}
